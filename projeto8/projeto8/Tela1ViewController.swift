18a078da1cebff9b371898c018a99d1bc3fa4a16
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-11-17 19:18:38 -0300

    navegando entre telas
    ;

--- projeto8/projeto8/Tela1ViewController.swift
+++ projeto8/projeto8/Tela1ViewController.swift
@@ -5,7 +5,13 @@
 //  Created by LUNARA MORENA CUNHA on 17/11/22.
 //
 
+import UIKit
+
 class Tela1ViewController : UIViewController
 {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
     
 }
